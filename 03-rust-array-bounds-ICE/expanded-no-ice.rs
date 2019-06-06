c47a1ed175d953b4bc530a797435bb09ed83d73c
Alex Macleod <alex@macleod.io>
2019-06-06 19:10:34 +0100

    03 rust ICE

---
+++ 03-rust-array-bounds-ICE/expanded-no-ice.rs
@@ -0,0 +1,16 @@
+#![feature(prelude_import)]
+#![no_std]
+#[prelude_import]
+use std::prelude::v1::*;
+#[macro_use]
+extern crate std;
+fn main() {
+    {
+        ::std::io::_print(::std::fmt::Arguments::new_v1(
+            &["", "\n"],
+            &match (&[1, 2, 3][4],) {
+                (arg0,) => [::std::fmt::ArgumentV1::new(arg0, ::std::fmt::Debug::fmt)],
+            },
+        ));
+    };
+}
